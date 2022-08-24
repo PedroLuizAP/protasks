7e3ee87cf448cf0ae9209e2a4797dcb18a0063d9
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-08-24 11:59:10 -0300

    Created base from delete, edit, title and priority

--- protasks/src/App.js
+++ protasks/src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import {useState} from 'react';
+import "./App.css";
+import { useState } from "react";
 
-let initialState =[
+let initialState = [
   {
     id: 1,
     description: "First Task",
@@ -9,40 +9,79 @@ let initialState =[
   {
     id: 2,
     description: "Second Task",
-  }
-
+  },
 ];
 
 function App() {
-
-  const [tasks, setTasks] = useState(initialState)
+  const [tasks, setTasks] = useState(initialState);
 
   //const tasks = ;
 
-  function addTask(e){
+  function addTask(e) {
     e.preventDefault();
 
     const task = {
-      id: document.getElementById('id').value,
-      description: document.getElementById('description').value
-    }
+      id: document.getElementById("id").value,
+      description: document.getElementById("description").value,
+    };
 
-    setTasks([...tasks, {...task}]);
+    setTasks([...tasks, { ...task }]);
   }
   return (
     <>
-      <form>
-        <input id="id" type="text" placeholder="id"/>
-        <input id="description" type="text" placeholder="description"/>
-        <button onClick={addTask}>+ Task</button>
+      <form className="row g-3">
+        <div className="col-md-6">
+          <label for="inputId4" className="form-label">Id</label>
+          <input id="id" type="text" placeholder="id" className="form-control" />
+        </div>
+
+        <div className="col-md-6">
+          <label for="inputDescription4" className="form-label">Description</label>
+          <input id="description" type="text" placeholder="description" className="form-control" />
+        </div>
+
+        <hr />
+        <div className="col-12" >
+          <button className="btn btn-outline-secondary" onClick={addTask}>+ Task</button>
+        </div> 
       </form>
-      
+
       <div className="mt-3">
-        <ul className="list-group">
-          {tasks.map(task => (
-            <li key={task.id} className="list-group-item">{task.id}-{task.description}</li>
+          {tasks.map((task) => (
+            <div key={task.id} className="card mb-2 shadow-sm">
+              <div className="card-body">
+                <div className="d-flex justify-content-between">
+                <h5 className="card-title">
+                  <span className="badge bg-secondary me-1">
+                    {task.id}
+                  </span>
+                  - title
+                </h5>
+                <h6>
+                  priority: 
+                  <span className="ms-1 text-black">
+                    <i className="me-1 far fa-smile"/>
+                    regular
+                  </span>
+                </h6>                
+                </div>              
+                <p key={task.id} className="card-text">
+                  {task.id}-{task.description}
+                </p>
+
+                <div className="d-flex justify-content-end pt-2 m-0 border-top">
+                  <button className="btn btn-outline-primary me-2 btn-sm" >
+                    <i className="fas fa-pen me-2"/>
+                    edit
+                  </button>
+                  <button className="btn btn-outline-danger me-2 btn-sm" >
+                  <i className="fas fa-trash me-2"/>
+                    delete
+                  </button>
+                </div>
+              </div>
+            </div>         
           ))}
-        </ul>
       </div>
     </>
   );
