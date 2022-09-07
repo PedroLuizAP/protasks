7ec6ce4582e3e4c5b099a574ae0fe598233a6bae
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-09-07 20:49:45 -0300

    Create protask API

---
+++ protasks.Interface/Controllers/TaskController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+namespace protasks.Interface.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+        }
+        [HttpPut]
+
+        public void Put()
+        {
+        }
+        [HttpPost]
+
+        public void Post()
+        {
+        }
+        [HttpDelete]
+
+        public void Delete()
+        {
+        }
+    }
+}
