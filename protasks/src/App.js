b62eb1492650cbe13af4b68c8e18acc38f55dcf4
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-10-20 18:37:32 -0300

    create modal and adjust methods

--- protasks/src/App.js
+++ protasks/src/App.js
@@ -4,8 +4,13 @@ import { useEffect } from "react";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 import api from "./api/taks";
+import { Button, Modal } from "react-bootstrap"
 
 function App() {
+  const [ShowTaskModal, setShowTaskModal] = useState(false);
+
+  const handleTaskModal = () => setShowTaskModal(!ShowTaskModal);
+
   const [tasks, setTasks] = useState([]);
   const [task, setTask] = useState({ id: 0 });
 
@@ -14,6 +19,10 @@ function App() {
     return response.data;
   }
 
+const newTask = () =>{
+  setTask({ id: 0 });
+  handleTaskModal();
+}
 
   useEffect(() => {
     const getTasks = async () => {
@@ -24,6 +33,7 @@ function App() {
   }, []);
 
   const addTask = async (task) => {
+    handleTaskModal();
     const response = await api.post("task", task);
     setTasks([...tasks, response.data]);
   }
@@ -35,19 +45,21 @@ function App() {
     }
   };
 
-  function cancelTask() {
+  const cancelTask = () => {
     setTask({ id: 0 });
+    handleTaskModal();
   };
 
-  function editTask(id) {
+  const editTask = (id) => {
     const task = tasks.filter(task => task.id === id);
-
     setTask(task[0]);
+    handleTaskModal();
   };
   const updateTask = async (task) => {
+    handleTaskModal();
     const response = await api.put(`task/${task.id}`, task);
 
-    const {id} = response.data;
+    const { id } = response.data;
 
     setTasks(tasks.map((item) => item.id === id ? task : item));
     setTask({ id: 0 })
@@ -57,17 +69,36 @@ function App() {
 
   return (
     <>
-      <TaskForm
-        addTask={addTask}
-        updateTask={updateTask}
-        cancelTask={cancelTask}
-        selectedTask={task}
-        tasks={tasks} />
+      <div className="d-flex justify-content-between align-items-end mt-2 pb-3 border-bottom border-2">
+
+        <h1 className="m-0 p-0">
+          Tasks {task.id !== 0 ? task.id : ""}
+        </h1>
+        <Button variant="outline-secondary" onClick={newTask}>
+          <i className="fas fa-plus"></i>
+        </Button>
+      </div>
 
       <TaskList
         tasks={tasks}
         deleteTask={deleteTask}
         editTask={editTask} />
+
+
+      <Modal show={ShowTaskModal} onHide={handleTaskModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TaskForm
+            addTask={addTask}
+            updateTask={updateTask}
+            cancelTask={cancelTask}
+            selectedTask={task}
+            tasks={tasks} />
+        </Modal.Body>
+      </Modal>
+
     </>
   );
 }
