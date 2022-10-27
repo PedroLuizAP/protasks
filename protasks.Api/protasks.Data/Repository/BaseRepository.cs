53c33b5b9ca6ef53e0356766c60a61e34bcb3d5e
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-10-27 17:56:35 -0300

    create BaseRepository

---
+++ protasks.Api/protasks.Data/Repository/BaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using protasks.Data.Context;
+using protasks.Domain.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protasks.Data.Repository
+{
+    public class BaseRepository : IBaseRepository
+    {
+        private readonly DataContext _context;
+        public BaseRepository(DataContext context) => _context = context;
+        public void Add<T>(T entity) where T : class => _context.Add(entity);
+        public void Delete<T>(T entity) where T : class => _context.Remove(entity);
+        public void DeleteRange<T>(T entity) where T : class => _context.RemoveRange(entity);
+        public void Update<T>(T entity) where T : class => _context.Update(entity);
+        public async Task<bool> SaveChangesAsync() => await _context.SaveChangesAsync() > 0;
+    }
+}
