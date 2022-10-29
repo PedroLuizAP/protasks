2d90b1abdb87eab34211421a3e4a6e4669fab8df
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-10-29 11:54:54 -0300

    create TaskRepository

---
+++ protasks.Api/protasks.Data/Repository/TaskRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using protasks.Data.Context;
+using protasks.Domain.Entities;
+using protasks.Domain.Interfaces.Repository;
+
+namespace protasks.Data.Repository
+{
+    public class TaskRepository : BaseRepository, ITaskRepository
+    {
+        private readonly DataContext _context;
+
+        public TaskRepository(DataContext context) : base(context) => _context = context;
+
+        public async Task<TaskModel[]> GetAllAsync()
+        {
+            IQueryable<TaskModel> query = _context.Tasks;
+
+            return await query.AsNoTracking().ToArrayAsync();
+        }
+
+        public async Task<TaskModel> GetByIdAsync(long id)
+        {
+            IQueryable<TaskModel> query = _context.Tasks;
+
+            return await query.AsNoTracking().SingleOrDefaultAsync(task => task.Id == id);
+        }
+
+        public async Task<TaskModel> GetByTitleAsync(string title)
+        {
+            IQueryable<TaskModel> query = _context.Tasks;
+
+            return await query.AsNoTracking().SingleOrDefaultAsync(task => task.Title == title);
+        }
+    }
+}
