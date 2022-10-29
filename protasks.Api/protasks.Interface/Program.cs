22d6e70cdf9ec3069b16fa0d8ac898c09ed2b85d
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-10-29 11:55:43 -0300

    Interface to real class

--- protasks.Api/protasks.Interface/Program.cs
+++ protasks.Api/protasks.Interface/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using protasks.Data.Context;
+using protasks.Data.Repository;
+using protasks.Domain.Interfaces.Repository;
+using protasks.Domain.Interfaces.Services;
+using protasks.Domain.Services;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<IBaseRepository, BaseRepository>();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<ITaskService, TaskService>();
+
+
 builder.Services.AddControllers().AddJsonOptions(option =>
 {
     option.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
