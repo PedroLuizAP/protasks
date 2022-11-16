6d453e3c93711aeb32744e12f869a7c37d8ddcd4
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-16 18:41:51 -0300

    adjust DashBoard Mock

--- protasks/src/pages/dashboard/Dashboard.jsx
+++ protasks/src/pages/dashboard/Dashboard.jsx
@@ -1,12 +1,54 @@
 import React from "react";
 import TitlePage from "../../components/TitlePage";
+import {Card, Row, Col} from "react-bootstrap";
 
 export default function Dashboard() {
   return (
     <>
-      <TitlePage title="Dashboard"/>
-      <div>
-            cards here
+      <TitlePage title="Dashboard" />
+      <div className="mt-3">
+        <Row>
+          <Col>
+            <Card border="success">
+              <Card.Header>Current Custumer</Card.Header>
+              <Card.Body>
+                <Card.Title>
+                  <h1 className="text-center">25</h1>
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card border="secondary">
+              <Card.Header>Total Tasks</Card.Header>
+              <Card.Body>
+                <Card.Title>
+                  <h1 className="text-center">256</h1>
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card border="warning">
+              <Card.Header> Main Task</Card.Header>
+              <Card.Body>
+                <Card.Title>
+                  <h1 className="text-center">25</h1>
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card border="danger">
+              <Card.Header>Late Tasks</Card.Header>
+              <Card.Body>
+                <Card.Title>
+                  <h1 className="text-center">2</h1>
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </>
   );
