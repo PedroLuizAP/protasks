a0203b1e455e910a8fc5dd7c9f9f15753f1ddb4a
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-16 18:31:49 -0300

    adjust NavMenu routes

--- protasks/src/components/Menu.jsx
+++ protasks/src/components/Menu.jsx
@@ -11,10 +11,10 @@ export default function Menu() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link activeClassName="active" as={NavLink} to="/custumer">
+            <Nav.Link activeClassName="active" as={NavLink} to="/custumer/list">
               Custumoer
             </Nav.Link>
-            <Nav.Link activeClassName="active" as={NavLink} to="/tasks">
+            <Nav.Link activeClassName="active" as={NavLink} to="/tasks/list">
               Tasks
             </Nav.Link>
           </Nav>
