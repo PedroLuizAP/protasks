987596064e1f8b033724e220e61a4674b244fd6b
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-16 18:31:31 -0300

    adjust Routes

--- protasks/src/App.js
+++ protasks/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Custumer from './pages/custumer/Custumer';
 import Dashboard from "./pages/dashboard/Dashboard";
+import CustumerForm from './pages/custumer/CustumerForm';
 function App() {
   return (
     <Switch>
       <Route path="/" exact component={Dashboard} />
-      <Route path="/custumer" component={Custumer} />
-      <Route path="/tasks" component={Task} />
+      <Route path="/custumer/list" component={Custumer} />
+      <Route path="/custumer/detail/:id?" component={CustumerForm} />
+      <Route path="/tasks/list" component={Task} />
     </Switch>
   );
 }
