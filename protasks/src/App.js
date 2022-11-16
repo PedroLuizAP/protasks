1097abaee574ffee08b6739163fddfae4f6a5f68
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-16 18:41:40 -0300

    create Page Not Found

--- protasks/src/App.js
+++ protasks/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Custumer from './pages/custumer/Custumer';
 import Dashboard from "./pages/dashboard/Dashboard";
 import CustumerForm from './pages/custumer/CustumerForm';
+import PageNotFound from './pages/dashboard/PageNotFound';
 function App() {
   return (
     <Switch>
@@ -11,6 +12,7 @@ function App() {
       <Route path="/custumer/list" component={Custumer} />
       <Route path="/custumer/detail/:id?" component={CustumerForm} />
       <Route path="/tasks/list" component={Task} />
+      <Route component={PageNotFound} />
     </Switch>
   );
 }
---
+++ protasks/src/pages/dashboard/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1 className="mt-4 mb-4 text-center">Page Not Found</h1>
+    </div>
+  );
+}
