dcbec189fd38c7087ae6b2ccc6f7ede58b9e1beb
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-20 12:23:26 -0300

    Adjust PageNotFound

--- protasks/src/pages/dashboard/PageNotFound.tsx
+++ protasks/src/pages/dashboard/PageNotFound.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function PageNotFound() {
+ const PageNotFound = () => {
   return (
     <div>
       <h1 className="mt-4 mb-4 text-center">Page Not Found</h1>
     </div>
   );
 }
+
+export default PageNotFound;
