344fc8851258316ad095151f6b53f9014fe609b0
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-22 08:53:46 -0300

    convert TaskList to TS

---
+++ protasks/src/pages/tasks/TaskList.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import TaskItem from "./TaskItem";
+import { TaskListProps } from './../../model/task';
+
+const TaskList: React.FC<TaskListProps> = ({ tasks, editTask, handleConfirmModal }: TaskListProps) => {
+    return (
+        <div className="mt-3">
+            {tasks.map((task) => (
+                <TaskItem
+                    key={task.id}
+                    task={task}
+                    handleConfirmModal={handleConfirmModal}
+                    editTask={editTask} />
+            ))}
+        </div>
+    );
+}
+
+export default TaskList;
