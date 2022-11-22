6cf90b82236cbb89c32728497d46b89be6fd7710
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-22 09:21:51 -0300

    add Props

--- protasks/src/model/tasksProps.ts
+++ protasks/src/model/tasksProps.ts
@@ -1,8 +1,20 @@
-import { task } from "./task";
+import { ITask } from "./task";
 
 export interface TaskItemProps {
-    task: task;
+    task: ITask;
     editTask: (id: number) => void;
     handleConfirmModal: (id: number) => void;
-  }
-  
+};
+
+export interface TaskFormProps {
+    selectedTask: ITask;
+    updateTask: (task: ITask) => void;
+    addTask: (task: ITask) => void;
+    cancelTask: () => void;
+};
+
+export interface TaskListProps {
+    tasks: ITask[];
+    editTask: (id: number) => void;
+    handleConfirmModal: (id: number) => void;
+};
