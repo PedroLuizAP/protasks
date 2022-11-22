cbe737ee5f741e42d675b8e475f0b98c0c8f6cad
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-11-22 09:22:19 -0300

    convert TaskForms to TS

--- protasks/src/pages/tasks/TaskForm.tsx
+++ protasks/src/pages/tasks/TaskForm.tsx
@@ -1,63 +1,65 @@
 import { useState, useEffect } from 'react';
+import { TaskFormProps } from '../../model/tasksProps';
+import { ITask, Priority } from './../../model/task';
 
-const initialTask = {
+const initialTask: ITask = {
     id: 0,
     title: "",
-    priority: 0,
+    priority: Priority.Notdefinied,
     description: ""
 }
 
-export default function TaskForm(props) {
-    const [task, setTask] = useState(thisTask());
+const TaskForm: React.FC<TaskFormProps> = ({ selectedTask, updateTask, addTask, cancelTask }: TaskFormProps) => {
+    const [task, setTask] = useState<ITask>(thisTask());
 
     useEffect(() => {
-        if (props.selectedTask.id !== 0)
-            setTask(props.selectedTask);
-    }, [props.selectedTask]);
+        if (selectedTask.id !== 0)
+            setTask(selectedTask);
+    }, [selectedTask]);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        if (props.selectedTask?.id !== 0)
-            props.updateTask(task);
+        if (selectedTask?.id !== 0)
+            updateTask(task);
         else
-            props.addTask(task);
+            addTask(task);
 
         setTask(initialTask);
     }
 
-    const inputTextHandler = (e) => {
+    const handleValue = (e: any) => {
         const { name, value } = e.target;
         setTask({ ...task, [name]: value });
     };
 
-    const cancelTask = (e) => {
+    const handleCancelTask = (e:React.FormEvent<HTMLButtonElement>) => {
         e.preventDefault();
 
-        props.cancelTask();
+        cancelTask();
 
         setTask(initialTask);
     };
 
-    function thisTask() {
-        if (props.selectedTask.id !== 0)
-            return props.selectedTask;
+    function thisTask():ITask {
+        if (selectedTask.id !== 0)
+            return selectedTask;
         else
             return initialTask;
     }
 
     return (
         <>
-            
+
             <form className="row g-3" onSubmit={handleSubmit}>
                 <div className="col-md-6">
                     <label className="form-label">Title</label>
-                    <input id="title" type="text" placeholder="title" className="form-control" name="title" value={task.title} onChange={inputTextHandler} />
+                    <input id="title" type="text" placeholder="title" className="form-control" name="title" value={task.title} onChange={handleValue} />
                 </div>
 
                 <div className="col-md-6">
                     <label className="form-label">Priority</label>
-                    <select id="priority" className="form-select" name="priority" value={task.priority} onChange={inputTextHandler}>
+                    <select id="priority" className="form-select" name="priority" value={task.priority} onChange={handleValue}>
                         <option defaultValue="Not definied">Select...</option>
                         <option value="Low">Low</option>
                         <option value="Normal">Normal</option>
@@ -70,7 +72,7 @@ export default function TaskForm(props) {
 
                 <div className="col-md-12">
                     <label className="form-label">Description</label>
-                    <textarea id="description" type="text" placeholder="description" className="form-control" name="description" value={task.description} onChange={inputTextHandler} />
+                    <textarea id="description" placeholder="description" className="form-control" name="description" value={task.description} onChange={handleValue} />
                     <hr />
 
                 </div>
@@ -89,7 +91,7 @@ export default function TaskForm(props) {
 
                                     Save
                                 </button>
-                                <button className="btn btn-outline-danger" onClick={cancelTask}>
+                                <button className="btn btn-outline-danger" onClick={handleCancelTask}>
                                     <i className="fas fa-times me-2"></i>
 
                                     Cancel
@@ -102,3 +104,5 @@ export default function TaskForm(props) {
         </>
     );
 }
+
+export default TaskForm;
