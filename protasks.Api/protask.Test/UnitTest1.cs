34b67aca9a8aaf0d0f2fd85ab57c14522d12f070
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-12-11 01:45:44 -0300

    create a base for xUnit

---
+++ protasks.Api/protask.Test/UnitTest1.cs
@@ -0,0 +1,35 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using protasks.Data.Context;
+using protasks.Data.Repository;
+using protasks.Domain.Entities;
+
+namespace protask.Test
+{
+    public class UnitTest1
+    {
+        private DbContextOptions<DataContext> _dbContextOptions;
+        private SqliteConnection _connection;
+        public UnitTest1()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<DataContext>().UseInMemoryDatabase("TaskControllerTest").ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning)).Options;
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            using (var context = new DataContext(_dbContextOptions))
+            {
+                //mock
+                context.Add(new TaskModel { Title = "TesteUnit", Description = "Teste" });
+
+                context.SaveChanges();
+
+                TaskRepository taskRepository = new(context);
+
+                var teste = await taskRepository.GetAllAsync();
+            }
+        }
+    }
+}
