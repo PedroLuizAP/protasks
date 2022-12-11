255ee54d27235c98891949ef89eb2294be35e39b
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-12-11 10:40:13 -0300

    create ContextHelper

---
+++ protasks.Api/protask.Test/Helpers/ContextHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using protasks.Data.Context;
+using protasks.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protask.Test.Helpers
+{
+    internal class ContextHelper
+    {
+        internal DbContextOptions<DataContext> GetOptionsBuilder() => new DbContextOptionsBuilder<DataContext>().UseInMemoryDatabase("TaskControllerTest").ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning)).Options;
+
+        internal void MockContext(DataContext context)
+        {
+            context.Add(new TaskModel { Title = "TesteUnit", Description = "Teste", Priority = Priority.Low });
+
+            context.SaveChanges();
+        }        
+    }
+}
