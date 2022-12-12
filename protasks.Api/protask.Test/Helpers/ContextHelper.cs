6ad92c895922a78de69d6a72a2ed2eb0d60a58dc
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-12-12 18:32:47 -0300

    Adjust ContextHelper to Static

--- protasks.Api/protask.Test/Helpers/ContextHelper.cs
+++ protasks.Api/protask.Test/Helpers/ContextHelper.cs
@@ -2,23 +2,18 @@
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using protasks.Data.Context;
 using protasks.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace protask.Test.Helpers
 {
-    internal class ContextHelper
+    internal static class ContextHelper
     {
-        internal DbContextOptions<DataContext> GetOptionsBuilder() => new DbContextOptionsBuilder<DataContext>().UseInMemoryDatabase("TaskControllerTest").ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning)).Options;
+        internal static DbContextOptions<DataContext> GetOptionsBuilder() => new DbContextOptionsBuilder<DataContext>().UseInMemoryDatabase("TaskControllerTest").ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning)).Options;
 
-        internal void MockContext(DataContext context)
+        internal static void MockContext(this DataContext context)
         {
             context.Add(new TaskModel { Title = "TesteUnit", Description = "Teste", Priority = Priority.Low });
 
             context.SaveChanges();
-        }        
+        }
     }
 }
