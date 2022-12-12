135f28518f2f2505a29b4060ac785a884e71effc
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-12-12 18:33:15 -0300

    create BaseTest

---
+++ protasks.Api/protask.Test/Tests/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using protask.Test.Helpers;
+using protasks.Data.Context;
+
+namespace protask.Test.Tests
+{
+    internal abstract class BaseTest
+    {
+        private DbContextOptions<DataContext> _dbContextOptions;
+        protected DataContext DataContext { get; set; }
+        protected BaseTest()
+        {
+            _dbContextOptions = ContextHelper.GetOptionsBuilder();
+
+            DataContext = new(_dbContextOptions);
+
+            DataContext.MockContext();
+        }
+    }
+}
