00402582fd666366e464d5f71a5ff1e3b32085d4
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-12-15 10:31:32 -0300

    create Example Test with complex Object

---
+++ protasks.Api/protask.Test/TestData/TaskTestData.cs
@@ -0,0 +1,26 @@
+﻿using protasks.Domain.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protask.Test.TestData
+{
+    public class TaskTestData
+    {
+        public static TaskTestData Current { get; } = new TaskTestData();
+        public List<TaskModel> Choices { get; set; }
+
+        public TaskTestData()
+        {
+            Choices = new List<TaskModel>
+            {
+                new TaskModel {  Title="teste"},
+                new TaskModel {  Title = "xUniTest"}
+            };
+        }
+
+    }
+}
---
+++ protasks.Api/protask.Test/Tests/Services/TaskServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using protask.Test.TestData;
+using protasks.Domain.Entities;
+using System.Collections;
+
+namespace protask.Test.Tests.Services
+{
+    public class TaskServiceTest : BaseTest
+    {
+        public static TaskModel RepeatedTask => new TaskModel() { Title = "TesteUnit" };
+
+        [Theory]
+        [MemberData(nameof(GetUserChoiceTestData))]
+        internal async Task GetAllAsync_Test_NotEmpty(TaskModel task)
+        {
+
+        }
+
+        public static IEnumerable<object[]> GetUserChoiceTestData()
+        {
+            yield return new object[] { TaskTestData.Current.Choices[0]};
+        }
+    }
+}
