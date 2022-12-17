553bc7351b3ffc7b5c7b9618fb9ecbd400d5f428
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-12-17 12:31:57 -0300

    create TitleError

---
+++ protasks.Api/protask.Test/TestData/Task/TitleError.cs
@@ -0,0 +1,20 @@
+﻿using protasks.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protask.Test.TestData.Task
+{
+    public class TitleError : BaseTaskTestData
+    {
+        public TitleError()
+        {
+            Current.Tasks = new List<TaskModel>
+            {
+                new TaskModel() { Title = "TesteUnit" }
+            };
+        }
+    }
+}
