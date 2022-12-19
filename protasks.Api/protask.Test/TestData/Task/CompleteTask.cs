f5ed868a7d3b5f0817c6b51bca59efbe71cad2cd
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-12-19 00:01:39 -0300

    create CompleteTask Data

---
+++ protasks.Api/protask.Test/TestData/Task/CompleteTask.cs
@@ -0,0 +1,15 @@
+﻿using protasks.Domain.Entities;
+
+namespace protask.Test.TestData.Task
+{
+    public class CompleteTask : BaseTaskTestData
+    {
+        public CompleteTask()
+        {
+            Current.Tasks = new List<TaskModel>
+            {
+                new TaskModel() { Title = "NewTaskComplete", CreationDate = DateTime.Now, Id = 1, Priority = Priority.Low, Description = "Base Test" }
+            };
+        }
+    }
+}
