2dfb6b169e8ee27b3a4be43ad041b2748972e289
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-12-20 11:00:00 -0300

    create TaskWIthoutId

---
+++ protasks.Api/protask.Test/TestData/Task/TaskWithoutId.cs
@@ -0,0 +1,20 @@
+﻿using protasks.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protask.Test.TestData.Task
+{
+    public class TaskWithoutId : BaseTaskTestData
+    {
+        public TaskWithoutId()
+        {
+            Current.Tasks = new List<TaskModel>
+            {
+                new TaskModel() { Title = "No Id", CreationDate = DateTime.Now, Priority = Priority.Low, Description = "Base Test" }
+            };
+        }
+    }
+}
