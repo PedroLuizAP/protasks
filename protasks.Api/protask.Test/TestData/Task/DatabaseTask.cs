9d45fe7d24426b9d101aed88fc08a18ba79e97a7
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-12-23 14:12:43 -0300

    create DatabaseTask

---
+++ protasks.Api/protask.Test/TestData/Task/DatabaseTask.cs
@@ -0,0 +1,20 @@
+﻿using protasks.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protask.Test.TestData.Task
+{
+    public class DatabaseTask : BaseTaskTestData
+    {
+        public DatabaseTask()
+        {
+            Current.Tasks = new List<TaskModel>
+            {
+                new TaskModel { Id = 2, Title = "Conclude", Description = "Conclude", Priority = Priority.Low }
+            };
+        }
+    }
+}
