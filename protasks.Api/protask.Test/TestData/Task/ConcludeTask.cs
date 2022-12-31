0121a3da7094db15bda3fa49e4d1f350f8d61142
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2022-12-31 00:05:00 -0300

    create ConcludeTask

---
+++ protasks.Api/protask.Test/TestData/Task/ConcludeTask.cs
@@ -0,0 +1,20 @@
+﻿using protasks.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protask.Test.TestData.Task
+{
+    internal class ConcludeTask :BaseTaskTestData
+    {
+        public ConcludeTask()
+        {
+            Current.Tasks = new List<TaskModel>
+            {
+                new TaskModel() { Title = "NewTaskComplete", CreationDate = DateTime.Now, ConclusionDate = DateTime.Now, Id = 1, Priority = Priority.Low, Description = "Base Test" }
+            };
+        }
+    }
+}
