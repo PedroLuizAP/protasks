902457ddea87806674750e2a17f87d0acab01302
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-01-27 18:13:44 -0300

    adjust contructor

--- protasks.Api/protask.Test/Tests/Repository/TaskRepositoryTest.cs
+++ protasks.Api/protask.Test/Tests/Repository/TaskRepositoryTest.cs
@@ -5,10 +5,8 @@ namespace protask.Test.Tests.Repository
     public class TaskRepositoryTest : BaseTest
     {
         private readonly TaskRepository _taskRepository;
-        public TaskRepositoryTest() : base()
-        {
-            _taskRepository = new(DataContext);
-        }
+        public TaskRepositoryTest() : base() => _taskRepository = new(DataContext);
+        
 
         [Fact]
         internal async Task GetAllAsync_Test_NotEmpty()
