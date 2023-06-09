b058d1795bd1c25867288ab047d9e5667b62e3c5
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-06-08 22:01:01 -0300

    adjust BaseTaskTestData

--- protasks.Api/protask.Test/TestData/Task/BaseTaskTestData.cs
+++ protasks.Api/protask.Test/TestData/Task/BaseTaskTestData.cs
@@ -1,19 +1,13 @@
 ﻿using protasks.Domain.Entities;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace protask.Test.TestData.Task
 {
     public class BaseTaskTestData : IEnumerable<object[]>
     {
         public static BaseTaskTestData Current { get; set; } = new();
-        public List<TaskModel> Tasks { get; set; }
-        public BaseTaskTestData() { }
+        public List<TaskModel> Tasks { get; set; } = new();
+        public  BaseTaskTestData() { }
 
         public IEnumerator<object[]> GetEnumerator()
         {
